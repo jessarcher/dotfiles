62aefc15a25639bb57d8129fd815527042b4690b
Jess Archer <jess@jessarcher.com>
2016-11-15 22:46:52 +1000

    Add login motd script

---
+++ scripts/login.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf $CYAN
+echo '                _________'
+echo '               |\\W#####M\'
+echo '               |M\\W#####M\'
+echo '               |#M\\W#####M\'
+echo '               |##M\\W#####M\'
+echo '               A###M\\W#####M\'
+echo '      _______,a#####M|\W#####M\'
+echo '      \W############W  \W#####M\'
+echo '       \W##########W    \W#####M\'
+echo '        `YW#####W7"      \W#####M\'
+echo '           ¯¯¯¯¯          ¯¯¯¯¯¯¯¯'
+echo
+printf $GREEN
+echo 'Greetings Jess! Here is a vimtip for you:'
+echo
+fortune vimtips
+echo
+printf $NC
--- zshrc
+++ zshrc
@@ -43,3 +43,5 @@ fi
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 base16_ocean
+
+~/.dotfiles/scripts/login.sh
