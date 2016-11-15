2abe0cf274dd35db8736bd40e0ed1342f210ab63
Jess Archer <jarcher@infoxchange.org>
2016-11-15 14:34:14 +1000

    Update lock script to work with ffmpeg on work computer

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -2,7 +2,7 @@
 
 image_file=/tmp/screen_lock.png
 resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
-# filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor' 
-filters='noise=alls=10,gblur=sigma=8'
+filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor'
+#filters='noise=alls=10,gblur=sigma=8'
 ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 -vf "$filters" "$image_file"
 i3lock -e -d -i "$image_file"
