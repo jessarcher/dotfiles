c930bee81b99718e5c9ca1f76e29d68027938207
Jess Archer <jess@jessarcher.com>
2016-11-20 23:50:35 +1000

    Pause dunst notifications while locked

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -7,11 +7,20 @@ filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor'
 #filters='noise=alls=10,gblur=sigma=8'
 ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 -vf "$filters" "$image_file"
 
-# Turn the screen off 5 seconds after locking
 revert() {
     xset dpms 0 0 0
+
+    # Resume notifications
+    pkill -USR2 dunst
 }
 trap revert SIGHUP SIGINT SIGTERM
+
+# Turn the screen off 5 seconds after locking
 xset +dpms dpms 5 5 5
+
+# Pause notifications
+pkill -USR1 dunst
+
 i3lock -n -e -i "$image_file"
+
 revert
