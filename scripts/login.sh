f06912c1cef4339f8f1d2dcce7ab836e180549dd
Jess Archer <jess@jessarcher.com>
2022-03-01 20:17:56 +1000

    Update logo

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-CYAN='\033[0;36m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
 
-#printf $CYAN
 printf '
-                _________
-               |\\\W#####M\
-               |M\\\W#####M\
-               |#M\\\W#####M\
-               |##M\\\W#####M\
-               A###M\\\W#####M\
-      _______,a#####M|\W#####M\
-      \W############W  \W#####M\
-       \W##########W    \W#####M\
-        `YW#####W7"      \W#####M\
-           ¯¯¯¯¯          ¯¯¯¯¯¯¯¯
-' | lolcat
+               ⢸⣦⡈⠻⣿⣿⣿⣿⣿⣿⣷⣦⡀
+               ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
+               ⢸⣿⣿⣿⣿⣦⡈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
+               ⢸⣿⣿⣿⣿⣿⣿⣦⠈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
+            ⢀⣀⣴⣿⣿⣿⣿⣿⣿⣿⡿  ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
+    ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁    ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
+      ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠉         ⠈⠛⠿⣿⣿⣿⣿⣿⣿⣦⡀
+' | lolcat --truecolor
+
 printf '
-      Greetings Jess, welcome back!
-' | lolcat
+        Greetings Jess, welcome back!
+' | lolcat --truecolor
+
 echo
-# printf $GREEN
-# echo
-# fortune vimtips
-# echo
-# printf $NC
