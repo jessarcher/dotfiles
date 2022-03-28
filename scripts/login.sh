48003c21d68a9a5872bbdc3fbdc6fcec7af64f3d
Jess Archer <jess@jessarcher.com>
2022-03-28 10:05:40 +1000

    Refactor for readability

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+os=`cat /etc/fedora-release | awk '{printf "%s %s", $1, $3}'`
+rootFree=`df -h | grep root | awk '{print $4}' | tr -d '\n'`
+homeFree=`df -h | grep home | awk '{print $4}' | tr -d '\n'`
+memAvailable=`free -m | grep "Mem" | awk '{printf "%.1fG", $7/1024}'`
+cpuTemp=`sensors | grep CPU | awk '{print $2}' | sed 's/+//' | tr -d '\n'`
+loadAvg=`cat /proc/loadavg | awk '{printf "%s %s %s", $1, $2, $3}'`
+
 printf "
         ⢸⣦⡈⠻⣿⣿⣿⣶⣄          ⣸ ⣯ ⣫ ⣫  ⣵ ⣟ ⣏ ⣿ ⣯ ⣟
         ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄
-  ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄        `cat /etc/fedora-release | awk '{printf "%s %s", $1, $3}'`
-  ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄     `df -h | grep root | awk '{print $4}' | tr -d '\n'`   `df -h | grep home | awk '{print $4}' | tr -d '\n'`   `free -m | grep "Mem" | awk '{printf "%.1fG", $7/1024}'`   `sensors | grep CPU | awk '{print $2}' | sed 's/+//' | tr -d '\n'`  辰`cat /proc/loadavg | awk '{printf "%s %s %s", $1, $2, $3}'`
+  ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄        $os
+  ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄     $rootFree   $homeFree   $memAvailable   $cpuTemp  辰$loadAvg
 
 " | lolcat --truecolor --seed=22 --spread=6
