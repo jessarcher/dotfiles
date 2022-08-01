74c0c9e0c1fd9b9b967904bb8aae510cc382d01c
Jess Archer <jess@jessarcher.com>
2022-08-01 21:30:55 +1000

    Add webcam install script

---
+++ scripts/install-webcam.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+sudo dnf install gphoto2 ffmpeg-free v4l-utils kernel-devel kernel-headers
+
+git clone git@github.com:umlaeute/v4l2loopback.git /tmp/v4l2loopback --depth=1
+cd /tmp/v4l2loopback
+
+make clean
+make && sudo make install
+sudo depmod -a
+rm -rf /tmp/v4l2loopback
+
+echo 'options v4l2loopback devices=1 card_label="V4L2 Webcam" exclusive_caps=1' > /etc/modprobe.d/v4l2loopback.conf
+
+sudo modprobe v4l2loopback
