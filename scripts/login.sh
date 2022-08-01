2861e56d1c13e22593ae10067be4f3feebea2613
Jess Archer <jess@jessarcher.com>
2022-08-01 20:23:29 +1000

    Update login script

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 os=`cat /etc/fedora-release | awk '{printf "%s %s", $1, $3}'`
-rootFree=`df -h | grep root | awk '{print $4}' | tr -d '\n'`
-homeFree=`df -h | grep home | awk '{print $4}' | tr -d '\n'`
-memAvailable=`free -m | grep "Mem" | awk '{printf "%.1fG", $7/1024}'`
+rootAvail=`df -h --output=avail / | tail -n1 | tr -d ' \n'`
+rootSize=`df -h --output=size / | tail -n1 | tr -d ' \n'`
+memAvailable=`free -m | grep "Mem" | awk '{printf "%.1fG", $7/1000}'`
+memTotal=`free -m | grep "Mem" | awk '{printf "%.fG", $2/1000}'`
 cpuTemp=`sensors | grep CPU | awk '{print $2}' | sed 's/+//' | tr -d '\n'`
 loadAvg=`cat /proc/loadavg | awk '{printf "%s %s %s", $1, $2, $3}'`
 
@@ -11,6 +12,6 @@ printf "
         ⢸⣦⡈⠻⣿⣿⣿⣶⣄          ⣸ ⣯ ⣫ ⣫  ⣵ ⣟ ⣏ ⣿ ⣯ ⣟
         ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄
   ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄        $os
-  ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄     $rootFree   $homeFree   $memAvailable   $cpuTemp  辰$loadAvg
+  ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄     $rootAvail/$rootSize   $memAvailable/$memTotal   $cpuTemp  辰$loadAvg
 
 " | lolcat --truecolor --seed=22 --spread=6
