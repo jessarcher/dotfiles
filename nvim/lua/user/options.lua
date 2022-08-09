9d7276242158720ae7d959cc265a391ff888eb63
Jess Archer <jess@jessarcher.com>
2022-08-09 21:10:20 +1000

    Update Neovim options

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,38 +1,36 @@
-local opt = vim.opt
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.title = true
+vim.opt.termguicolors = true
+vim.opt.spell = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = false
+vim.opt.breakindent = true -- maintain indent when wrapping indented lines
+vim.opt.list = true -- enable the below listchars
+vim.opt.listchars = { tab = '▸ ', trail = '·' }
+vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
+vim.opt.mouse = 'a' -- enable mouse for all modes
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.clipboard = 'unnamedplus' -- Use Linux system clipboard
+vim.opt.confirm = true -- ask for confirmation instead of erroring
+vim.opt.undofile = true -- persistent undo
+vim.opt.backup = true -- automatically save a backup file
+vim.opt.backupdir:remove('.') -- keep backups out of the current directory
+vim.opt.shortmess:append({ I = true }) -- disable the splash screen
+vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.signcolumn = 'yes:2'
+vim.opt.showmode = false
+vim.opt.updatetime = 4001 -- Set updatime to 1ms longer than the default to prevent polyglot from changing it
+vim.opt.redrawtime = 10000 -- Allow more time for loading syntax on large files
 
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.signcolumn = 'yes:2'
-opt.relativenumber = true
-opt.number = true
-opt.breakindent = true
-opt.termguicolors = true
-opt.undofile = true
-opt.spell = true
-opt.title = true
-opt.ignorecase = true
-opt.smartcase = true
-opt.wildmode = 'longest:full,full'
-opt.wrap = false
-opt.list = true
-opt.listchars = 'tab:▸ ,trail:·'
-opt.mouse = 'a'
-opt.splitright = true
-opt.splitbelow = true
-opt.scrolloff = 8
-opt.sidescrolloff = 8
-opt.clipboard = 'unnamedplus' -- Use Linux system clipboard
-opt.confirm = true
-opt.backup = true
-opt.backupdir = vim.fn.stdpath('data') .. '/backup//'
-opt.updatetime = 4001 -- Set updatime to 1ms longer than the default to prevent polyglot from changing it
-opt.redrawtime = 10000 -- Allow more time for loading syntax on large files
-opt.showmode = false
-opt.fillchars = 'eob: ' -- Hide `~` on empty lines
-opt.shortmess:append({ I = true })
-
-local highlight_group = vim.api.nvim_create_augroup('Blah', { clear = true })
+local highlight_group = vim.api.nvim_create_augroup('GeneralColorSchemeTweaks', { clear = true })
 vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function()
     vim.cmd('hi SpellBad gui=undercurl guifg=none')
