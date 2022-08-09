a7ecc6f0ffcd55f6a09aefe4ccc114113066ae9c
Jess Archer <jess@jessarcher.com>
2022-08-09 21:10:42 +1000

    Update init.lua logo

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,14 @@
 --
---                           ⢸⣦⡈⠻⣿⣿⣿⣿⣿⣿⣷⣦⡀
---                           ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
---                           ⢸⣿⣿⣿⣿⣦⡈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
---                           ⢸⣿⣿⣿⣿⣿⣿⣦⠈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
---                        ⢀⣀⣴⣿⣿⣿⣿⣿⣿⣿⡿  ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
---                ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁    ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣦⡀
---                  ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠉         ⠈⠛⠿⣿⣿⣿⣿⣿⣿⣦⡀
+--                            ⢸⣦⡈⠻⣿⣿⣿⣶⣄
+--                            ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄
+--                      ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄
+--                      ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄
 --
---  Personal vim configuration of Jess Archer <jess@jessarcher.com>
+-- Personal Neovim configuration of Jess Archer <jess@jessarcher.com>
 
 require('user.plugins')
 require('user.options')
 require('user.keymaps')
 require('user.misc')
+
+-- vim: nospell
