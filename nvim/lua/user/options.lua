bac9e04078b4adba40602b1d3b6b7f08b7a929b7
Jess Archer <jess@jessarcher.com>
2022-08-13 23:10:35 +1000

    Update options

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,6 +1,8 @@
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.title = true
