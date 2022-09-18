4d5c5a09437f84d2bad85eea3d0628ed344b5a52
Jess Archer <jess@jessarcher.com>
2022-09-18 14:44:39 +1000

    Update packer bootstrapping and init

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,14 +1,20 @@
 -- Install packer
-local is_bootstrap = false
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function ()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 -- Initialize packer
 require('packer').init({
+  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
   display = {
     open_fn = function()
       return require('packer.util').float({ border = 'solid' })
@@ -319,7 +325,7 @@ use({
 })
 
 -- Automatically install plugins on first run
-if is_bootstrap then
+if packer_bootstrap then
   require('packer').sync()
 end
 
