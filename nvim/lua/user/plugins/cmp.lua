c52fc8b48cd1f2faa09714cd1280f59cb7f94342
Jess Archer <jess@jessarcher.com>
2023-09-26 10:43:10 +1000

    Delay heavy plugins

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -1,5 +1,6 @@
 return {
   'hrsh7th/nvim-cmp',
+  event = "InsertEnter",
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lsp-signature-help',
--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,5 +1,6 @@
 return {
   'neovim/nvim-lspconfig',
+  event = 'VeryLazy',
   dependencies = {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
