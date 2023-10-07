823571a958a90710696da4044ed0df5492fd125a
Luis Guillermo Echenique <elepistemedev@gmail.com>
2023-10-07 12:17:40 -0400

    FIX: agregando themes a zsh y organizando ejecución

--- scripts/post-install-fedora.sh
+++ scripts/post-install-fedora.sh
@@ -27,3 +27,5 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf -y check-update
 sudo dnf -y install code
+
+./fedora-setup.sh
