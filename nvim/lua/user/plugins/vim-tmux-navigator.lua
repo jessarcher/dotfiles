b01ac753946606e7e936fd97a81a204f70fa55cd
Jess Archer <jess@jessarcher.com>
2025-11-19 20:06:49 +1000

    Update vim-tmux-navigator config

--- nvim/lua/user/plugins/vim-tmux-navigator.lua
+++ nvim/lua/user/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,24 @@
 -- Navigate seamlessly between Vim windows and Tmux panes.
 
-return { 'christoomey/vim-tmux-navigator' }
+return {
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  init = function()
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
+  keys = {
+    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+  }
+}
+
