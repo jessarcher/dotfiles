e3636d0e282b4df961bed81a7b1418732eb5d632
Jess Archer <jess@jessarcher.com>
2025-11-19 20:10:50 +1000

    Update gitsigns config

--- nvim/lua/user/plugins/gitsigns.lua
+++ nvim/lua/user/plugins/gitsigns.lua
@@ -19,8 +19,8 @@ return {
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-      delay = 1000,
-      ignore_whitespace = false,
+      delay = 500,
+      ignore_whitespace = true,
     },
     signs = {
       add          = { text = '│' },
