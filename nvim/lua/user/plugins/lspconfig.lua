de96a8cbb96a1bb4b5be505176f80ee671c271fa
Jess Archer <jess@jessarcher.com>
2026-05-19 12:05:01 +1000

    Update mason config

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -15,7 +15,16 @@ return {
         height = 0.8,
       },
     })
-    require('mason-lspconfig').setup({ automatic_installation = true })
+    require('mason-lspconfig').setup({
+      ensure_installed = {
+        'intelephense',
+        'vtsls',
+        'tailwindcss',
+        'jsonls',
+        'lua_ls',
+      },
+      automatic_enable = false,
+    })
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
