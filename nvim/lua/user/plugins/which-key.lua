8998fc3e56035b69305e8e23466bdaf6dbf9afb5
Jess Archer <jess@jessarcher.com>
2026-05-19 14:10:31 +1000

    Delay which-key

--- nvim/lua/user/plugins/which-key.lua
+++ nvim/lua/user/plugins/which-key.lua
@@ -5,7 +5,9 @@ return {
   event = 'VeryLazy',
   config = function()
     local wk = require('which-key')
-    wk.setup({})
+    wk.setup({
+      delay = 400,
+    })
 
     wk.add({
       { '<leader>t', group = 'Test' },
